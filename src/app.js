0e502e00194d8bff4b5c3804935637f6490ed0cf
gaolei <2503404258@qq.com>
2019-01-11 15:47:22 +0800

    完成按钮的基本样式

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import Button from "./button";
+import Vue from "vue";
+Vue.component("g-button",Button);
+new Vue({
+    el: "#app"
+})
---
+++ src/button.vue
@@ -0,0 +1,30 @@
+<template>
+    <button class="g-button">按钮</button>
+</template>
+
+<script>
+    export default {
+        name: "button"
+    }
+</script>
+
+<style scoped>
+    .g-button {
+        font-size: var(--font-size);
+        height: var(--button-height);
+        background-color: var(--button-bg);
+        border-radius: var(--border-radius);
+        padding: 0 1em;
+        color: var(--color);
+        border: 1px solid var(--border-color);
+    }
+    .g-button:hover {
+        border-color: var(--border-color-hover);
+    }
+    .g-button:active {
+        background-color: var(--button-active-bg);
+    }
+    .g-button:focus {
+        outline: none;
+    }
+</style>
