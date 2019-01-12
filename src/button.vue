4eafd16b438cf5c44347e95434a1daf6e90c5be3
gaolei <2503404258@qq.com>
2019-01-12 09:01:09 +0800

    增加icon和iconPosition属性

--- src/button.vue
+++ src/button.vue
@@ -1,15 +1,36 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="`icon-${iconPosition}`">
+        <svg v-if="icon" class="icon" aria-hidden="true">
+            <use :xlink:href="`#${icon}`"></use>
+        </svg>
+       <div class="content">
+           <slot></slot>
+       </div>
+    </button>
 </template>
 
 <script>
     export default {
-        name: "button"
+        name: "button",
+        props: {
+            icon:String,
+            iconPosition: {
+                type:String,
+                default: "left",
+                validator(value) {
+                    return ["left","right"].includes(value)
+                }
+            }
+        }
     }
 </script>
 
 <style scoped>
     .g-button {
+        display: inline-flex;
+        vertical-align: middle;
+        justify-content: center;
+        align-items: center;
         font-size: var(--font-size);
         height: var(--button-height);
         background-color: var(--button-bg);
@@ -27,4 +48,21 @@
     .g-button:focus {
         outline: none;
     }
+
+    .icon-right > .content {
+        order: 1;
+    }
+
+    .icon-right > .icon {
+        order: 2;
+    }
+
+    .icon-left > .content {
+        order: 2;
+    }
+
+    .icon-left > .icon {
+        order: 1;
+    }
+
 </style>
