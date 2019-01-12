f096e63d526cd49792ed6bf6cb2cc96d1059e459
gaolei <2503404258@qq.com>
2019-01-12 22:49:43 +0800

    用chai和chai-spies测试

--- src/app.js
+++ src/app.js
@@ -12,3 +12,84 @@ new Vue({
         loading:true
     }
 })
+
+//单元测试
+import chai from "chai";
+import spies from "chai-spies";
+chai.use(spies)
+const expect = chai.expect;
+{
+
+    const Construtor = Vue.extend(Button);
+    const vm = new Construtor({
+        propsData: {
+            icon: "icon-setting"
+        }
+    }).$mount();
+    const useElement = vm.$el.querySelector("use");
+    const href = useElement.getAttribute("xlink:href")
+    expect(href).to.equal("#icon-setting")
+
+}
+
+{
+    const Construtor = Vue.extend(Button);
+    const vm = new Construtor({
+        propsData: {
+            icon: "icon-setting",
+            loading: true
+        }
+    }).$mount();
+    const useElement = vm.$el.querySelector("use");
+    const href = useElement.getAttribute("xlink:href")
+    expect(href).to.equal("#icon-loading")
+}
+
+{
+    const div = document.createElement("div");
+    document.body.appendChild(div);
+    const Construtor = Vue.extend(Button);
+    const vm = new Construtor({
+        propsData: {
+            icon: "icon-setting"
+        }
+    }).$mount(div);
+    const svgElement = vm.$el.querySelector("svg");
+    const {order} = window.getComputedStyle(svgElement)
+    expect(order).to.equal("1")
+    vm.$el.remove();
+    vm.$destroy()
+
+}
+
+{
+    const div = document.createElement("div");
+    document.body.appendChild(div);
+    const Construtor = Vue.extend(Button);
+    const vm = new Construtor({
+        propsData: {
+            icon: "icon-setting",
+            iconPosition: "right"
+        }
+    }).$mount(div);
+    const svgElement = vm.$el.querySelector("svg");
+    const {order} = window.getComputedStyle(svgElement)
+    expect(order).to.equal("2")
+    vm.$el.remove();
+    vm.$destroy()
+
+
+}
+
+{
+    const Construtor = Vue.extend(Button);
+    const vm = new Construtor({
+        propsData: {
+            icon: "icon-setting"
+        }
+    }).$mount();
+    const spy = chai.spy(() => {})
+    vm.$on("click",spy);
+    vm.$el.click()
+    expect(spy).to.have.been.called()
+}
