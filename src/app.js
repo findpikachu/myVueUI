4c872a75c6da6b3ba8690ff7adff0e7711b68cf8
gaolei <2503404258@qq.com>
2019-01-16 15:17:45 +0800

    添加input.vue

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@ import Button from "./button";
 import Icon from "./icon";
 import Vue from "vue";
 import ButtonGroup from "./button-group";
+import Input from "./input";
+
 
 Vue.component("g-button",Button);
 Vue.component("g-icon",Icon);
 Vue.component("g-button-group",ButtonGroup);
+Vue.component("g-input",Input);
 new Vue({
     el: "#app",
     data: {
@@ -13,83 +16,3 @@ new Vue({
     }
 })
 
-//单元测试
-import chai from "chai";
-import spies from "chai-spies";
-chai.use(spies)
-const expect = chai.expect;
-{
-
-    const Construtor = Vue.extend(Button);
-    const vm = new Construtor({
-        propsData: {
-            icon: "icon-setting"
-        }
-    }).$mount();
-    const useElement = vm.$el.querySelector("use");
-    const href = useElement.getAttribute("xlink:href")
-    expect(href).to.equal("#icon-setting")
-
-}
-
-{
-    const Construtor = Vue.extend(Button);
-    const vm = new Construtor({
-        propsData: {
-            icon: "icon-setting",
-            loading: true
-        }
-    }).$mount();
-    const useElement = vm.$el.querySelector("use");
-    const href = useElement.getAttribute("xlink:href")
-    expect(href).to.equal("#icon-loading")
-}
-
-{
-    const div = document.createElement("div");
-    document.body.appendChild(div);
-    const Construtor = Vue.extend(Button);
-    const vm = new Construtor({
-        propsData: {
-            icon: "icon-setting"
-        }
-    }).$mount(div);
-    const svgElement = vm.$el.querySelector("svg");
-    const {order} = window.getComputedStyle(svgElement)
-    expect(order).to.equal("1")
-    vm.$el.remove();
-    vm.$destroy()
-
-}
-
-{
-    const div = document.createElement("div");
-    document.body.appendChild(div);
-    const Construtor = Vue.extend(Button);
-    const vm = new Construtor({
-        propsData: {
-            icon: "icon-setting",
-            iconPosition: "right"
-        }
-    }).$mount(div);
-    const svgElement = vm.$el.querySelector("svg");
-    const {order} = window.getComputedStyle(svgElement)
-    expect(order).to.equal("2")
-    vm.$el.remove();
-    vm.$destroy()
-
-
-}
-
-{
-    const Construtor = Vue.extend(Button);
-    const vm = new Construtor({
-        propsData: {
-            icon: "icon-setting"
-        }
-    }).$mount();
-    const spy = chai.spy(() => {})
-    vm.$on("click",spy);
-    vm.$el.click()
-    expect(spy).to.have.been.called()
-}
---
+++ src/input.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="wrapper">
+        <input type="text" :value="value" :disabled="disabled" :readonly="readonly">
+        <template v-if="error">
+            <g-icon icon="icon-error" class="error"></g-icon>
+            <span class="errorMessage">{{error}}</span>
+        </template>
+    </div>
+</template>
+
+<script>
+    import Icon from "./icon";
+
+    export default {
+        name: "input",
+        components: {Icon},
+        props: {
+            value: {
+                type: String
+            },
+            disabled: {
+                type: Boolean,
+                default: false
+            },
+            readonly: {
+                type: Boolean,
+                default: false
+            },
+            error: String
+
+        }
+    }
+</script>
+
+<style scoped>
+    .wrapper > input {
+        height: var(--button-height);
+        border: 1px solid var(--border-color);
+        border-radius: var(--border-radius);
+        padding: 0 8px;
+    }
+
+    .wrapper > input:hover {
+        border-color: var(--border-color-hover);
+    }
+
+    .wrapper > input:focus {
+        box-shadow: inset 0 1px 3px var(--box-shadow-color);
+        outline: none;
+    }
+
+    .wrapper > input[disabled], .wrapper > input[readonly] {
+        cursor: not-allowed;
+    }
+    .error {
+        fill: red;
+    }
+    .errorMessage {
+        color: red;
+    }
+    .wrapper {
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
