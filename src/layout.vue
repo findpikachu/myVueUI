e065874edbd78de64e1d778fcab35620b8eba668
gaolei <2503404258@qq.com>
2019-01-23 10:05:08 +0800

    layout content footer sider 组件基本功能完成

---
+++ src/layout.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="layout" :class="{siderClass:hasSider}">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "layout",
+        data() {
+            return {
+                hasSider: false
+            }
+        },
+        mounted() {
+            this.$children.forEach(vm => {
+                if (vm.$options.name === "sider")
+                    this.hasSider = true
+            })
+        }
+    }
+</script>
+
+<style scoped>
+    .layout {
+        display: flex;
+        flex-direction: column;
+        flex-grow: 1;
+        border: 1px solid red;
+    }
+    .siderClass {
+        flex-direction: row;
+    }
+</style>
---
+++ src/sider.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "sider"
+    }
+</script>
+
+<style scoped>
+
+</style>
